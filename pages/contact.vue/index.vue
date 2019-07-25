cce20ec2772f3b40743bb03f8bb664181325388f
Dan Fulcher <me@danfulcher.co.uk>
2019-07-25 18:44:14 +0100

    init commit

---
+++ pages/contact.vue/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="contact.vue">
+
+    </div>
+</template>
+
+<style lang="sass" scoped>
+    @import './index.scss';
+</style>
+
+<script>
+
+
+export default {
+    name: 'Contact.vue',
+    data() {
+        return {
+            
+        };
+    },
+    
+    methods: {
+        
+    },
+    mounted() {
+        
+    },
+};
+</script>
